c82a6192f6745319ed54e369ab6ad509d7a99e38
hiks <kitnhiks@gmail.com>
2015-06-19 21:35:41 +0200

    twitter keys

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'aDUG6MmnTUtfD5bALIDpaG1Ls',
+  consumer_secret: 'jGwpxBXODgmSA2kqZpHo1BByHi2eh88U49sgSSE4jpINrsoS5f',
+  access_token_key: '3297011716-9pKplsdEOoSyALt7HxswiWfnZElnIBBklWdLn9A',
+  access_token_secret: 'hGdVNEp75oF0nhPyIv6YySZFEODgkRuC3p8idR7spuShu'
 });
 
 client.stream('statuses/filter', {track: '#bugshunt #challenge1'},  function(stream){
