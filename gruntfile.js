c86fe17d3a0af8a245caf7f33313df180caafb6e
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-19 20:24:43 +0200

    Add gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,92 @@
+'use strict';
+
+module.exports = function(grunt) {
+	// Unified Watch Object
+	var watchFiles = {
+		serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
+		mochaTests: ['tests/**/*.js']
+	};
+
+	// Project Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			serverJS: {
+				files: watchFiles.serverJS,
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+		},
+		jshint: {
+			all: {
+				src: watchFiles.clientJS.concat(watchFiles.serverJS),
+				options: {
+					jshintrc: true
+				}
+			}
+		},
+		nodemon: {
+			dev: {
+				script: 'server.js',
+				options: {
+					nodeArgs: ['--debug'],
+					ext: 'js,html',
+					watch: watchFiles.serverViews.concat(watchFiles.serverJS)
+				}
+			}
+		},
+		'node-inspector': {
+			custom: {
+				options: {
+					'web-port': 1337,
+					'web-host': 'localhost',
+					'debug-port': 5858,
+					'save-live-edit': true,
+					'no-preload': true,
+					'stack-trace-limit': 50,
+					'hidden': []
+				}
+			}
+		},
+		concurrent: {
+			default: ['nodemon', 'watch'],
+			debug: ['nodemon', 'watch', 'node-inspector'],
+			options: {
+				logConcurrentOutput: true,
+				limit: 10
+			}
+		},
+		mochaTest: {
+			src: watchFiles.mochaTests,
+			options: {
+				reporter: 'mocha-spec-cov-alt',
+				require: 'server.js'
+			}
+		}
+	});
+
+	// Load NPM tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Making grunt default to force in order not to break the project.
+	grunt.option('force', true);
+
+	// A Task for loading the configuration object
+	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
+		grunt.config.set('applicationJavaScriptFiles', 'server.js');
+	});
+
+	// Default task(s).
+	grunt.registerTask('default', ['lint', 'concurrent:default']);
+
+	// Debug task.
+	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
+
+	// Lint task(s).
+	grunt.registerTask('lint', ['jshint', 'csslint']);
+
+	// Test task.
+	grunt.registerTask('test', ['mochaTest']);
+};
