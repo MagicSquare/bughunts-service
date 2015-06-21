6defd30b3bf6ddba11f0bbfc1264ddf6d0676288
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-21 21:16:06 +0200

    Add "grunt antlr" command.

--- gruntfile.js
+++ gruntfile.js
@@ -1,93 +1,104 @@
 'use strict';
 
-module.exports = function(grunt) {
-	// Unified Watch Object
-	var watchFiles = {
-		serverJS: ['server.js', 'app/src/*.js'],
-		mochaTests: ['app/test/*.js']
-	};
+module.exports = function (grunt) {
+    // Unified Watch Object
+    var watchFiles = {
+        serverJS: ['server.js', 'app/src/*.js'],
+        mochaTests: ['app/test/*.js']
+    };
+    var antlrFiles = {
+        grammar: ['lang/BugHunts.g']
+    };
 
-	// Project Configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		watch: {
-			serverJS: {
-				files: watchFiles.serverJS,
-				tasks: ['jshint'],
-				options: {
-					livereload: true
-				}
-			}
-		},
-		jshint: {
-			all: {
-				src: watchFiles.serverJS,
-				options: {
-					jshintrc: true
-				}
-			}
-		},
-		nodemon: {
-			dev: {
-				script: 'server.js',
-				options: {
-					ext: 'js,html',
-					watch: watchFiles.serverJS
-				}
-			}
-		},
-		'node-inspector': {
-			custom: {
-				options: {
-					'web-port': 1337,
-					'web-host': 'localhost',
-					'debug-port': 5858,
-					'save-live-edit': true,
-					'no-preload': true,
-					'stack-trace-limit': 50,
-					'hidden': []
-				}
-			}
-		},
-		concurrent: {
-			default: ['nodemon', 'watch'],
-			debug: ['nodemon', 'watch', 'node-inspector'],
-			options: {
-				logConcurrentOutput: true,
-				limit: 10
-			}
-		},
-		mochaTest: {
-			src: watchFiles.mochaTests,
-			options: {
-				require: 'server.js'
-			}
-		}
-	});
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            serverJS: {
+                files: watchFiles.serverJS,
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jshint: {
+            all: {
+                src: watchFiles.serverJS,
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    ext: 'js,html',
+                    watch: watchFiles.serverJS
+                }
+            }
+        },
+        'node-inspector': {
+            custom: {
+                options: {
+                    'web-port': 1337,
+                    'web-host': 'localhost',
+                    'debug-port': 5858,
+                    'save-live-edit': true,
+                    'no-preload': true,
+                    'stack-trace-limit': 50,
+                    'hidden': []
+                }
+            }
+        },
+        concurrent: {
+            default: ['nodemon', 'watch'],
+            debug: ['nodemon', 'watch', 'node-inspector'],
+            options: {
+                logConcurrentOutput: true,
+                limit: 10
+            }
+        },
+        mochaTest: {
+            src: watchFiles.mochaTests,
+            options: {
+                require: 'server.js'
+            }
+        },
+        exec: {
+            antlr: {
+                command: 'java -Xmx500M -cp ./lang/antlr-4.5-complete.jar: org.antlr.v4.Tool -Dlanguage=JavaScript -o ./app/src ' + antlrFiles.grammar
+            }
+        }
+    });
 
-	// Load NPM tasks
-	require('load-grunt-tasks')(grunt);
+    // Load NPM tasks
+    require('load-grunt-tasks')(grunt);
 
-	// Making grunt default to force in order not to break the project.
-	grunt.option('force', true);
+    // Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
 
-	// A Task for loading the configuration object
-	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
-		grunt.config.set('applicationJavaScriptFiles', 'server.js');
-	});
+    // A Task for loading the configuration object
+    grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function () {
+        grunt.config.set('applicationJavaScriptFiles', 'server.js');
+    });
 
-	// Default task(s).
-	grunt.registerTask('default', ['lint', 'concurrent:default']);
+    // Default task(s).
+    grunt.registerTask('default', ['lint', 'concurrent:default']);
 
-	// Debug task.
-	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
+    // Debug task.
+    grunt.registerTask('debug', ['lint', 'concurrent:debug']);
 
-	// Lint task(s).
-	grunt.registerTask('lint', ['jshint']);
+    // Lint task(s).
+    grunt.registerTask('lint', ['jshint']);
 
-	// Build task(s).
-	grunt.registerTask('build', ['lint', 'loadConfig']);
+    // Build task(s).
+    grunt.registerTask('build', ['lint', 'loadConfig']);
 
-	// Test task.
-	grunt.registerTask('test', ['mochaTest']);
+    // Test task.
+    grunt.registerTask('test', ['mochaTest']);
+
+    // Antlr task.
+    grunt.registerTask('antlr', ['exec:antlr']);
 };
