6a6e1e500c00b0de443e90d06d250ebe0cd3cc4e
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-21 20:08:35 +0200

    Test challenge3 with real tweet structure.

--- app/test/twitter_extractor.tests.js
+++ app/test/twitter_extractor.tests.js
@@ -1,19 +1,15 @@
 'use strict';
 
 var should = require('should'),
-    twitter_extractor = require('../src/twitter_extractor');
+    twitter_extractor = require('../src/twitter_extractor'),
+    tweet_challenge3 = require('./resources/tweet_challenge3.js');
 
-var unknown_tweet = null,
-    tweet_challenge3 = null;
+var unknown_tweet = null;
 
 describe('tweet extractor', function () {
     before(function(done) {
         unknown_tweet = {
             text: 'blah blah blah'
-        };
-
-        tweet_challenge3 = {
-            text: '#bugshunt #challenge3 FO TL TL FO'
         };
 
         done();
@@ -26,13 +22,13 @@ describe('tweet extractor', function () {
     });
 
     it('should return the texte when receiving a good tweet', function (done) {
-        var tweetData = twitter_extractor.extractInstructions(tweet_challenge3);
+        var tweetData = twitter_extractor.extractInstructions(tweet_challenge3.tweet);
         tweetData.instructions.should.be.equal('FO TL TL FO');
         done();
     });
 
     it('should return the challenge id when receiving a good tweet', function (done) {
-        var tweetData = twitter_extractor.extractInstructions(tweet_challenge3);
+        var tweetData = twitter_extractor.extractInstructions(tweet_challenge3.tweet);
         tweetData.challenge.should.be.equal('3');
         done();
     });
