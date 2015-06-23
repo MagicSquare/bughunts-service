d2fdd5b8d49d1a69c5e73b2045a1b1639dca6ddf
hiks <kitnhiks@gmail.com>
2015-06-23 22:07:02 +0200

    uses now bugshunt_dev account for the answer

--- app/src/twitter_answer.js
+++ app/src/twitter_answer.js
@@ -3,18 +3,11 @@ var Twitter = require('twitter'),
 
 exports.listen = function(challenge) {
 
-    /*var client = new Twitter({ // MagicSquare_FR
-     consumer_key: 'aDUG6MmnTUtfD5bALIDpaG1Ls',
-     consumer_secret: 'jGwpxBXODgmSA2kqZpHo1BByHi2eh88U49sgSSE4jpINrsoS5f',
-     access_token_key: '3297011716-9pKplsdEOoSyALt7HxswiWfnZElnIBBklWdLn9A',
-     access_token_secret: 'hGdVNEp75oF0nhPyIv6YySZFEODgkRuC3p8idR7spuShu'
-     });*/
-
-    var client = new Twitter({ // TeamMagicSquare
-        consumer_key: 'hfSjot7L3dUL8mezoAn1gLoqh',
-        consumer_secret: 'xwNEnUfw9JK9uYvpdZscjOKQNVCE3T0D5nHVg5rbJio2Acch8d',
-        access_token_key: '3290556340-7rO4ZSN3JYTh1QlfLqdw2AhNx9aBsOcGjtHWR4X',
-        access_token_secret: 'AqbwApKLWAxpsjgduaIaY8JIHUPfWAoZB0kpZVBzOjnWc'
+    var client = new Twitter({ // bugshunt_dev
+        consumer_key: 'pnxVwUVRnNNPdVI4Zyd8QdgDt',
+        consumer_secret: 'mV7oNf2PORH2iXD2AUuLsxHrRGy8esP1Hvkf8cf01CsJJro1rP',
+        access_token_key: '3337904121-Glzb3mRseE0rN8UiU7UZ3xyEamDkZmnMYUhDRqX',
+        access_token_secret: 'pQu6Vit7sNKzNpqjeKQ0Gndd8u6B3b7JC8niKCgHTBZ7e'
     });
 
 // Filter tweet on hashtags #bugshunt and #challenge1
