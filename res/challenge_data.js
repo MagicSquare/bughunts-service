3f819da259f32a9a700bdbe4c453ed52e2e4bdab
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-23 23:16:25 +0200

    Dynamic resolution for image

--- res/challenge_data.js
+++ res/challenge_data.js
@@ -3,7 +3,7 @@ exports.map = [
     ['o','o','o','o','o'],
     ['o','o','o','g','o'],
     ['o','o','o','o','o'],
-    ['o','o','o','o','o']
+    ['l','o','o','o','o']
 ];
 
-exports.hashTag = '#challenge1';
+exports.hashTag = '#0x003D';
--- server.js
+++ server.js
@@ -4,15 +4,18 @@ var RestClient = require('node-rest-client').Client,
     twitter_question = require('./app/src/twitter_question'),
     challengeData = require('./res/challenge_data');
 
-var mapSquares = '';
+var mapSquares = '',
+    nbX = 0,
+    nbY = challengeData.map.length;
 challengeData.map.map(function (line) {
+    nbX = line.length;
     line.map(function (square) {
         mapSquares += square;
     });
 });
 
 var mapClient = new RestClient();
-mapClient.get('http://151.80.235.36:8000/8/7/13/' + mapSquares, function (data, response) {
+mapClient.get('http://151.80.235.36:8000/' + nbX + '/' + nbY +'/13/' + mapSquares, function (data, response) {
 
     var challenge = new Challenge(challengeData.hashTag, challengeData.map, data);
     twitter_question.ask(challenge);
