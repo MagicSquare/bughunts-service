8eba436bdd72b048eb337b0de4a276e5c85433bd
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-08-01 09:37:10 +0200

    Refactor challenge #0x002

--- app/res/challenges/0x0002.js
+++ app/res/challenges/0x0002.js
@@ -3,32 +3,20 @@ var challenge = require('bughunts-challenge');
 exports.name = '#0x0002';
 
 exports.squares = [
-    ['l','1','o','o','o'],
-    ['o','o','o','s','o'],
-    ['2','o','3','g','s'],
-    ['o','o','o','s','o'],
-    ['o','o','o','o','o']
+    ['s','o','o','o','o','g'],
+    ['o','o','o','o','s','s'],
+    ['o','s','o','o','o','o'],
+    ['o','o','o','s','o','o'],
+    ['o','o','s','o','o','o'],
+    ['o','o','o','o','s','o'],
+    ['o','o','o','o','l','s']
 ];
 
 exports.actors = [
     {
         type: 'l',
-        pos: new challenge.Point(0, 0),
-        dir: challenge.Game.DIR_RIGHT
-    },
-    {
-        type: 't',
-        pos: new challenge.Point(1, 0),
-        target : 2
-    },
-    {
-        type: 'm',
-        pos: new challenge.Point(0, 2),
-        dir: challenge.Game.DIR_RIGHT
-    },
-    {
-        type: 'w',
-        pos: new challenge.Point(2, 2)
+        pos: new challenge.Point(4, 6),
+        dir: challenge.Game.DIR_LEFT
     }
 ];
 
